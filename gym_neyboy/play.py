b9247a2a3139391c9f6f92fd94a884e156544099
fabito <fuechi@ciandt.com>
2018-08-09 18:07:53 -0300

    move play to main module

--- gym_neyboy/play.py
+++ gym_neyboy/play.py
@@ -10,7 +10,7 @@ import gym_neyboy
 
 def main(screen, args):
 
-    os.environ['GYM_NEYBOY_OBS_AS_BYTES'] = '1'
+    os.environ['GYM_NEYBOY_ENV_NON_HEADLESS'] = '1'
     env = gym.make(args.env)
     total_reward = 0
     total_steps = 0
@@ -28,18 +28,18 @@ def main(screen, args):
         observation, reward, done, info = env.step(action)
         total_reward += reward
 
-        if args.color:
-            renderer = ColourImageFile(screen, observation, height=args.height)
-        else:
-            renderer = ImageFile(observation, height=args.height, colours=screen.colours)
+        # if args.color:
+        #     renderer = ColourImageFile(screen, observation, height=args.height)
+        # else:
+        #     renderer = ImageFile(observation, height=args.height, colours=screen.colours)
+        #
+        # image, colours = renderer.rendered_text
+        # for (i, line) in enumerate(image):
+        #     screen.centre(line, i, colour_map=colours[i])
 
-        image, colours = renderer.rendered_text
-        for (i, line) in enumerate(image):
-            screen.centre(line, i, colour_map=colours[i])
-
-        # screen.print_at('{0:.2f}'.format(reward), 0, 0)
-        # screen.print_at('{0:.2f}'.format(total_reward), 0, 1)
-        # screen.print_at('done: {}'.format(done), 0, 2)
+        screen.print_at('{0:.2f}'.format(reward), 0, 0)
+        screen.print_at('{0:.2f}'.format(total_reward), 0, 1)
+        screen.print_at('done: {}'.format(done), 0, 2)
 
         screen.refresh()
 
