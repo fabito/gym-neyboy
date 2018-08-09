aa55f0472fb3ea8a6bc34607fd6f32316edc731c
fabito <fuechi@ciandt.com>
2018-08-09 17:23:03 -0300

    add missing packages

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import sys
+
+if sys.version_info.major != 3:
+    print('This Python is only compatible with Python 3, but you are running '
+          'Python {}. The installation will likely fail.'.format(sys.version_info.major))
 
 setup(name='gym_neyboy',
       version='0.0.1',
+      packages=[package for package in find_packages()],
       extras_require={
             "terminal": ["asciimatics"],
       },
